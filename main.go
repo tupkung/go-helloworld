b1124402197276c04513ecfc9311927bb6f1d7ec
Pheerawit Wasinphongwanit <pheerawit.was@gec.co.th>
2018-11-24 13:08:49 +0700

    change to get port from environment variable

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8888"
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world")
 	})
 
-	log.Fatal(http.ListenAndServe(":80", mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
